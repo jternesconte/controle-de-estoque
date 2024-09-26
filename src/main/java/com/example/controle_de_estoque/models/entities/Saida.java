73ced3e8569f660fa07daaa8a13bac5c35a653d6
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-09-25 21:31:06 -0300

    feat: Criado entity para Entrada e Saida

---
+++ src/main/java/com/example/controle_de_estoque/models/entities/Saida.java
@@ -0,0 +1,52 @@
+package com.example.controle_de_estoque.models.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "entradas")
+public class Saida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produtoId;
+
+    @Column(name = "quantidade", nullable = false)
+    private int quantidade;
+
+    public Saida() {
+
+    }
+
+    public Saida(Produto produtoId, int quantidade) {
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Produto getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Produto produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
