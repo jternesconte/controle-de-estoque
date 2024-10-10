6a67b1684cd85da50b2c03a6bdd887e4b8fb2186
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-10-09 22:16:21 -0300

    feat: criado getAll e adição de entradas

--- backend/src/main/java/com/example/controle_de_estoque/controllers/EntradaController.java
+++ backend/src/main/java/com/example/controle_de_estoque/controllers/EntradaController.java
@@ -1,10 +1,13 @@
 package com.example.controle_de_estoque.controllers;
 
-import com.example.controle_de_estoque.models.repositories.CategoriaRepository;
+import com.example.controle_de_estoque.models.entities.Entrada;
+import com.example.controle_de_estoque.models.entities.Produto;
 import com.example.controle_de_estoque.models.repositories.EntradaRepository;
 import com.example.controle_de_estoque.models.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class EntradaController {
@@ -13,8 +16,35 @@ public class EntradaController {
     private ProdutoRepository produtoRepository;
 
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    private EntradaRepository entradaRepository;
 
-    @Autowired
-    EntradaRepository entradaRepository;
+    public List<Entrada> getAllEntradas() {
+        return (List<Entrada>) entradaRepository.findAll();
+    }
+
+    public Entrada novaEntrada(int produtoId, Entrada entradaDetalhes) {
+        Produto produto = produtoRepository.findById(produtoId)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));;
+
+        if (produto.getCategoriaId() == null) {
+            throw new RuntimeException("Produto não possui uma categoria associada");
+        }
+
+        int novaQuantidade = produto.getQuantidade() + entradaDetalhes.getQuantidade();
+        produto.setQuantidade(novaQuantidade);
+        produto.setNome(produto.getNome());
+        produto.setDescricao(produto.getDescricao());
+        produto.setPreco(produto.getPreco());
+        produto.setQuantidade(produto.getQuantidade());
+        produto.setFlAtivo(produto.getFlAtivo());
+
+        produtoRepository.save(produto);
+
+        Entrada entrada = new Entrada(
+          produto,
+          entradaDetalhes.getQuantidade()
+        );
+
+        return entradaRepository.save(entrada);
+    }
 }
--- backend/src/main/java/com/example/controle_de_estoque/ws/EntradaWs.java
+++ backend/src/main/java/com/example/controle_de_estoque/ws/EntradaWs.java
@@ -1,10 +1,31 @@
 package com.example.controle_de_estoque.ws;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.controle_de_estoque.controllers.EntradaController;
+import com.example.controle_de_estoque.controllers.ProdutoController;
+import com.example.controle_de_estoque.models.entities.Entrada;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/entrada")
 public class EntradaWs {
+
+    @Autowired
+    private ProdutoController produtoController;
+
+    @Autowired
+    private EntradaController entradaController;
+
+    @GetMapping("/getAllEntradas")
+    public @ResponseBody List<Entrada> getAllEntradas() {
+        return entradaController.getAllEntradas();
+    }
+
+    @PostMapping("/novaEntrada/{produtoId}")
+    public Entrada novaEntrada(@PathVariable int produtoId, @RequestBody Entrada entradaDetalhes) {
+        return entradaController.novaEntrada(produtoId, entradaDetalhes);
+    }
 
 }
