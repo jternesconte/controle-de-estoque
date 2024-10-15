5dd7093ed657da383dcacb92bb43e93922ab32a5
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-14 21:50:26 -0300

    feat: adiciona os components de product

--- front/src/pages/product.tsx
+++ front/src/pages/product.tsx
@@ -1,9 +1,18 @@
+import { CreateProduct } from "@/components/createProduct/createProduct"
 import { TableProducts } from "@/components/table/tableProducts"
+import { Toaster } from "@/components/ui/toaster"
+import { Link } from "react-router-dom"
 
 export const Product = () => {
+
   return (
-    <main>
+    <main className="max-w-7xl m-auto py-10">
+      <div className="flex justify-between">
+        <CreateProduct />
+        <Link to="/categories" className="underline font-medium">Categorias</Link>
+      </div>
       <TableProducts />
+      <Toaster />
     </main>
   )
 }
