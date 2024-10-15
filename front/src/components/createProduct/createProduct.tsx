818a5097e2222885e4c64d28a68ed5f2944867dd
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-14 21:46:01 -0300

    improviment: adiciona label e corrige rota de criar produto

--- front/src/components/createProduct/createProduct.tsx
+++ front/src/components/createProduct/createProduct.tsx
@@ -1,78 +1,82 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from "../ui/button"
-import { useEffect, useState } from "react";
-import { useProducts } from "@/hooks/use-product";
+import { useState } from "react";
+import { useProducts } from "@/hooks/useProducts";
 import { Input } from "../ui/input";
+import { useCategories } from "@/hooks/useCategories";
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 export const CreateProduct = () => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
-  const [price, setPrice] = useState<number>(0);
+  // const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
+  const [price, setPrice] = useState("");
   // const [price, setPrice] = useState<number>(0);
   const [quantity, setQuantity] = useState<number>(0);
 
-  const { addProduct, fetchCategories, category } = useProducts();
+
+  const { addProduct, dialogOpenProducts, setDialogOpenProducts } = useProducts();
+  const { categories } = useCategories();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Chamando a função do hook para adicionar o produto
+    const categoria_id = Number(selectedCategoryId)
+
     await addProduct(
       {
         nome: name,
         descricao: description,
-        preco: price,
+        preco: Number(price),
         quantidade: quantity,
-        categoria_id: selectedCategoryId,
+        categoria_id,
         fl_ativo: "S"
       },
-      selectedCategoryId // Usando o id da categoria selecionada
+      Number(selectedCategoryId)
     );
 
-    // Resetando os campos do formulário
+    setDialogOpenProducts(false);
     setName('');
     setDescription('');
-    setPrice(0);
+    setPrice('');
     setQuantity(0);
-    setSelectedCategoryId(0); // Resetando a categoria selecionada
+    setSelectedCategoryId("");
   };
 
   return (
-    <Dialog>
+    <Dialog open={dialogOpenProducts} onOpenChange={setDialogOpenProducts}>
       <DialogTrigger>
         <Button className="mb-5">Adicionar produto</Button>
       </DialogTrigger>
 
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle>Criar produto</DialogTitle>
         </DialogHeader>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <label htmlFor="">nome</label>
           <Input
             placeholder="Nome"
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
           />
+          <label htmlFor="">descrição</label>
           <Input
             placeholder="Descrição"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             required
           />
+          <label htmlFor="">preço</label>
           <Input
             type="text"
             placeholder="Preço"
@@ -82,6 +86,7 @@ export const CreateProduct = () => {
             pattern="[0-9]*"
             required
           />
+          <label htmlFor="">quantidade</label>
           <Input
             type="number"
             placeholder="Quantidade"
@@ -91,20 +96,37 @@ export const CreateProduct = () => {
             required
           />
 
+          <label htmlFor="">categoria</label>
+          {/* <Select
+
+            required
+          >
+            <SelectTrigger >
+              <SelectValue placeholder="Categoria" value={selectedCategoryId}
+                onChange={(e) => setSelectedCategoryId(e.target.value)} />
+            </SelectTrigger>
+            <SelectContent>
+              {category.map((category) => (
+                <SelectItem key={category.id} value={(category.id).toString()}>
+                  {category.nome}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select> */}
           <select
             value={selectedCategoryId}
-            onChange={(e) => setSelectedCategoryId(Number(e.target.value))}
+            onChange={(e) => setSelectedCategoryId((e.target.value))}
             required
           >
             <option value="">Selecione uma categoria</option>
-            {category.map((category) => (
+            {categories.map((category) => (
               <option key={category.id} value={category.id}>
                 {category.nome}
               </option>
             ))}
           </select>
 
-          <Button type="submit">Add Product</Button>
+          <Button type="submit" className="mt-6">Adicionar Produto</Button>
         </form>
       </DialogContent>
     </Dialog>
