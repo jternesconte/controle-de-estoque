287cd043924b32e3029f39ea8337dff6073d92c2
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-14 21:44:17 -0300

    feat: create category component

---
+++ front/src/components/createCategory/createCategory.tsx
@@ -0,0 +1,65 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "../ui/button"
+import { useState } from "react";
+import { useCategories, useProducts } from "@/hooks/useCategories";
+import { Input } from "../ui/input";
+
+export const CreateCategory = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [dialogOpenCategories, setDialogOpenCategories] = useState(false);
+
+  const { addCategory } = useCategories();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Chamando a função do hook para adicionar o produto
+    await addCategory(
+      {
+        nome: name,
+        descricao: description,
+        fl_ativo: "S"
+      }
+    );
+
+    setName('');
+    setDescription('');
+  };
+
+  return (
+    <Dialog open={dialogOpenCategories} onOpenChange={setDialogOpenCategories}>
+      <DialogTrigger className="mb-5">
+        <Button>Adicionar categoria</Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Criar categoria</DialogTitle>
+        </DialogHeader>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <Input
+            placeholder="Nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          <Button type="submit">Adicionar Categoria</Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
