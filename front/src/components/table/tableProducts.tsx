53d8bb2d984ffd98793c38c3a6e837a287eb473c
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-18 00:11:17 -0300

    feat: adiciona entrada e saida a tabela de produto

--- front/src/components/table/tableProducts.tsx
+++ front/src/components/table/tableProducts.tsx
@@ -13,12 +13,15 @@ import { Button } from "../ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog"
 import { Input } from "../ui/input"
 import { Product } from "@/types/product";
+// import EntradaSaidaModal from "../entradaEsaida/entradaSaida";
+import { toast } from "@/hooks/use-toast";
 import { useState } from "react";
-import EntradaSaidaModal from "../entradaEsaida/entradaSaida";
 
 
 export function TableProducts() {
-  const [selectedProductId, setSelectedProductId] = useState<number | null>(null);
+  const [entrada, setEntrada] = useState<string>('');
+  const [saida, setSaida] = useState<string>('');
+  const [openEditDialogStock, setOpenEditDialogStock] = useState(false);
 
   const { products, loadingProducts, errorProducts, handleEditProduct, openEditDialog, setOpenEditDialog, selectedProduct, setSelectedProduct, editProduct } = useProducts()
 
@@ -34,13 +37,81 @@ export function TableProducts() {
   };
 
   const toggleProductActiveStatus = async (product: Product) => {
-    const updatedProduct = { ...product, flAtivo: !product.flAtivo }; // Inverte o estado de flAtivo
+    const updatedProduct = { ...product, flAtivo: !product.flAtivo };
 
     try {
-      await editProduct(updatedProduct); // Chama a função editProduct existente
+      await editProduct(updatedProduct);
     } catch (error) {
       console.error("Erro ao alternar o estado de flAtivo:", error);
     }
+  };
+
+  // ENTRADA / SAIDA
+
+  const handleSubmit = async () => {
+    if (selectedProduct) {
+      try {
+        let response;
+        if (entrada !== '') {
+          response = await fetch(`http://127.0.0.1:8080/api/entrada/novaEntrada/${selectedProduct.id}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ quantidade: entrada }),
+
+          });
+          setEntrada('')
+        } else if (saida !== '') {
+          response = await fetch(`http://127.0.0.1:8080/api/saida/novaSaida/${selectedProduct.id}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ quantidade: Number(saida) }),
+          });
+          setSaida('')
+        }
+        if (response && !response.ok) {
+          throw new Error('Erro ao processar a transação');
+        }
+
+        toast({
+          title: entrada !== '' ? "Entrada realizada com sucesso." : "Retirada realizada com sucesso.",
+        });
+
+        setOpenEditDialogStock(false)
+      } catch (error: any) {
+        console.error('Erro:', error.message);
+        toast({
+          title: error.message,
+        });
+      }
+    }
+  };
+
+  const handleEntradaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // Retém apenas os dígitos
+    setEntrada(value.replace(/\D/g, ''));
+  };
+
+  const handleSaidaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // Retém apenas os dígitos
+    setSaida(value.replace(/\D/g, ''));
+  };
+
+  const handlePrecoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // Retém apenas os dígitos e um único ponto decimal
+    const numericValue = value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');
+    setSelectedProduct({ ...selectedProduct, preco: numericValue });
+  };
+
+  const openStockModal = (product: Product) => {
+    setSelectedProduct(product);
+    setOpenEditDialogStock(true)
   };
 
   return (
@@ -49,34 +120,32 @@ export function TableProducts() {
         <TableHeader>
           <TableRow>
             <TableHead className="w-[250px]">Nome</TableHead>
-            {/* <TableHead>Descrição</TableHead> */}
-            <TableHead>Categoria</TableHead>
-            <TableHead className="text-right">Quantidade</TableHead>
-            <TableHead className="text-right">Preço</TableHead>
-            <TableHead className="text-right">Entrada/Saida</TableHead>
-            <TableHead className="text-right">Desativo/Ativo</TableHead>
-            <TableHead className="text-right">Editar</TableHead>
+            <TableHead className="">Categoria</TableHead>
+            <TableHead className="text-center">Preço</TableHead>
+            <TableHead className="text-center">Quantidade</TableHead>
+            <TableHead className="text-center">Entrada/Saida</TableHead>
+            <TableHead className="text-center">Desativo/Ativo</TableHead>
+            <TableHead className="text-center">Editar</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {products.map((product) => (
             <TableRow key={product.id}>
-              <TableCell className="font-medium">{product.nome}</TableCell>
-              {/* <TableCell>{product.descricao}</TableCell> */}
+              <TableCell className="font-medium w-[250px]">{product.nome}</TableCell>
               <TableCell>{product.categoriaId.nome}</TableCell>
 
-              <TableCell className="text-center translate-x-5">{product.quantidade}</TableCell>
-              <TableCell className="text-right">R$ {product.preco}</TableCell>
+              <TableCell className="text-center">R$ {product.preco}</TableCell>
+              <TableCell className="text-center">{product.quantidade}</TableCell>
 
-              <TableCell className="text-center translate-x-7">
+              <TableCell className="text-center">
+                <button onClick={() => openStockModal(product)}>Entrada/Saída</button>
+              </TableCell>
+
+              <TableCell className="text-center ">
                 <Switch checked={product.flAtivo} onCheckedChange={() => toggleProductActiveStatus(product)} />
               </TableCell>
 
-              <TableCell className="text-right">
-                <EntradaSaidaModal product={product} />
-              </TableCell>
-
-              <TableCell className="text-right">
+              <TableCell className="text-center">
                 <button onClick={() => handleEditProduct(product)}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
                 </button>
@@ -88,7 +157,44 @@ export function TableProducts() {
         </TableFooter>
       </Table>
 
-      <Dialog open={openEditDialog} onOpenChange={setOpenEditDialog}>
+      {/* {selectedProduct && (
+        <EntradaSaidaModal product={selectedProduct} onClose={closeModal} />
+      )} */}
+
+      <Dialog open={openEditDialogStock} onOpenChange={setOpenEditDialogStock}>
+
+        <DialogContent className="">
+          <DialogHeader>
+            <DialogTitle>Entrada e Saida</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col gap-4 bg-white px-20 py-10 rounded-lg">
+
+            <div className='text-center'>
+              <strong>Quantidade Atual:</strong> {selectedProduct?.quantidade}
+            </div>
+
+            <Input
+              type="text"
+              placeholder="Quantidade de Entrada"
+              value={entrada}
+              onChange={handleEntradaChange}
+              disabled={saida != ''}
+            />
+
+            <Input
+              type="text"
+              placeholder="Quantidade de Saída"
+              value={saida}
+              onChange={handleSaidaChange}
+              disabled={entrada != ''}
+            />
+
+            <Button onClick={handleSubmit}>Confirmar</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={openEditDialog} onOpenChange={setOpenEditDialog} >
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Editar Produto</DialogTitle>
@@ -117,10 +223,10 @@ export function TableProducts() {
             <div>
               <label htmlFor="">Preço</label>
               <Input
-                type="number"
+                type="text"
                 placeholder="quantidade"
                 value={selectedProduct?.preco}
-                onChange={(e) => setSelectedProduct({ ...selectedProduct, preco: e.target.value })}
+                onChange={handlePrecoChange}
                 required
               />
             </div>
